f8a3f4ec6b9106f5e94481ac1536c32d3bc18020
Sam Noel <noel.samuel.a@gmail.com>
2022-11-18 16:12:30 -0500

    fix: align types with other services

--- src/conversions.ts
+++ src/conversions.ts
@@ -40,6 +40,9 @@ const conversions = {
     name: 'Pure Food Guaranty and Indemnification Agreement (LOG)',
     urlName: 'pfgias',
     type: 'application/vnd.trellisfw.pfgia.1+json',
+    alternativeNames: [
+      'Signed Smithfield Pure Food Guaranty and Indemnification Agreement (PFGIA)',
+    ],
   },
   'Letter of Guarantee': {
     name: 'Letter of Guarantee',
@@ -66,7 +69,7 @@ const conversions = {
   },
   '100g Nutritional Information': {
     name: '100g Nutritional Information',
-    urlName: '100g-nutritional-information',
+    urlName: 'nutritional-information',
     type: 'application/vnd.trellisfw.nutritional-information.1+json',
     alternativeNames: ['Nutrition Information'],
   },
@@ -98,7 +101,7 @@ const conversions = {
   },
   'Gluten Statement': {
     name: 'Gluten Statement',
-    urlName: 'gluten-claim-statements',
+    urlName: 'gluten-statements',
     type: 'application/vnd.trellisfw.gluten-claim-statement.1+json',
     alternativeNames: ['Gluten Claim'],
   },
@@ -136,13 +139,18 @@ const conversions = {
   },
   'GFSI Certificate': {
     name: 'GFSI Certificate',
-    urlName: 'fsqa-certificates',
-    type: 'application/vnd.trellisfw.fsqa-certificates.1+json',
+    urlName: 'gfsi-certificates',
+    type: 'application/vnd.trellisfw.gfsi-certificate.1+json',
   },
   'Non-Ambulatory (3D/4D) Animal Statement': {
     name: 'Non-Ambulatory (3D/4D) Animal Statement',
     urlName: 'animal-statements',
     type: 'application/vnd.trellisfw.animal-statement.1+json',
+  },
+  'Specified Risk Materials (SRM) Statement / Audit': {
+    name: 'Specified Risk Materials (SRM) Statement / Audit',
+    urlName: 'srm-statements-audits',
+    type: 'application/vnd.trellisfw.srm-statement-audit.1+json',
   },
   'Specified Risk Materials (SRM) Audit': {
     name: 'Specified Risk Materials (SRM) Audit',
@@ -167,7 +175,7 @@ const conversions = {
   'Foreign Material Control Plan': {
     name: 'Foreign Material Control Plan',
     urlName: 'foreign-material-control-plans',
-    type: 'application/vnd.trellisfw.foreign-material-control-plans.1+json',
+    type: 'application/vnd.trellisfw.foreign-material-control-plan.1+json',
   },
   'Animal Welfare Audit': {
     name: 'Animal Welfare Audit',
@@ -225,15 +233,25 @@ const conversions = {
     urlName: 'tpa-corrective-actions',
     type: 'application/vnd.trellisfw.tpa-corrective-actions.1+json',
   },
+  'Third Party Food Safety GMP Audit': {
+    name: 'Third Party Food Safety GMP Audit',
+    urlName: 'tpa-food-safety-audits',
+    type: 'application/vnd.trellisfw.tpa-food-safety-audit.1+json',
+  },
+  'Third Party Animal Welfare Audit': {
+    name: 'Third Party Animal Welfare Audit',
+    urlName: 'tpa-animal-welfare-audits',
+    type: 'application/vnd.trellisfw.tpa-animal-welfare-audit.1+json',
+  },
+  'Third Party Animal Welfare Corrective Actions': {
+    name: 'Third Party Animal Welfare Corrective Actions',
+    urlName: 'tpa-animal-welfare-corrective-actions',
+    type: 'application/vnd.trellisfw.tpa-animal-welfare-corrective-actions.1+json',
+  },
   'W-8': {
     name: 'W-8',
     urlName: 'w-8s',
     type: 'application/vnd.trellisfw.w-8.1+json',
-  },
-  'Third Party Food Safety GMP Audit': {
-    name: 'Third Party Food Safety GMP Audit',
-    urlName: 'fsqa-audits',
-    type: 'application/vnd.trellisfw.fsqa-audit.1+json',
   },
   'Animal Welfare Corrective Actions': {
     name: 'Animal Welfare Corrective Actions',
@@ -244,6 +262,11 @@ const conversions = {
     name: 'Third Party Food Safety GMP Certificate',
     urlName: 'fsqa-certificates',
     type: 'application/vnd.trellisfw.fsqa-certificate.1+json',
+  },
+  'Signed Vendor Acknowledgement Form': {
+    name: 'Signed Vendor Acknowledgement Form',
+    urlName: 'signed-vendor-acknowledgement-forms',
+    type: 'application/vnd.trellisfw.signed-vendor-acknowledgement-form.1+json',
   },
   'Small Business Administration (SBA) Form': {
     name: 'Small Business Administration (SBA) Form',
@@ -259,6 +282,11 @@ const conversions = {
     name: 'E.Coli 0157:H7 Intervention Statement',
     urlName: 'ecoli-statements',
     type: 'application/vnd.trellisfw.ecoli-statement.1+json',
+  },
+  'E.Coli 0157:H7 Intervention Statement / Audit': {
+    name: 'E.Coli 0157:H7 Intervention Statement / Audit',
+    urlName: 'ecoli-intervention-statements-audits',
+    type: 'application/vnd.trellisfw.ecoli-intervention-statement-audits.1+json',
   },
   'Business License': {
     name: 'Business License',
